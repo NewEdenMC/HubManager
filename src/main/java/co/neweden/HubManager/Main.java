c17ca7139fa4e632e42bb8e66ecf2bcd82868344
aaron <AaronMcHale>
2017-09-21 18:44:12 +0100

    Added check to prevent falling into void

--- src/main/java/co/neweden/HubManager/Main.java
+++ src/main/java/co/neweden/HubManager/Main.java
@@ -2,20 +2,36 @@ package co.neweden.HubManager;
 
 import co.neweden.HubManager.JumpPads.JumpPads;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         Bukkit.getPluginManager().registerEvents(new JumpPads(getLogger(), getConfig().getConfigurationSection("jumpPads")), this);
+        if (getConfig().getBoolean("preventFallingIntoVoid", false)) {
+            Bukkit.getPluginManager().registerEvents(this, this);
+            getLogger().info("Prevent Falling Into Void check enabled");
+        }
     }
 
     @Override
     public void onDisable() {
-        HandlerList.unregisterAll(this);
+        HandlerList.unregisterAll((Plugin) this);
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (player.getLocation().getY() <= 0)
+            player.teleport(player.getWorld().getSpawnLocation());
     }
 
 }
