58452f51bc3f8c9793c6272a0757fd7c29d200bc
aaron <AaronMcHale>
2017-09-26 18:25:06 +0100

    Parsing Locations supports yaw and pitch

--- src/main/java/co/neweden/HubManager/Util.java
+++ src/main/java/co/neweden/HubManager/Util.java
@@ -13,7 +13,7 @@ public class Util {
 
     public static Location parseLocationFromString(String location, boolean worldRequired) throws IllegalArgumentException {
         Validate.notNull(location);
-        String genericError = "Tried to parse location \"" + location + "\" however it is not in the correct format, format should be \"X-COORD Y-COORD Z-COORD\" for example \"0 64 0\".  Specific error: ";
+        String genericError = "Tried to parse location \"" + location + "\" however it is not in the correct format, format should start with \"X-COORD Y-COORD Z-COORD\" and can be followed by \"YAW PITCH\" for example \"0 64 0\" or \"0 64 0 0 0\".  Specific error: ";
         String[] parts = location.split(" ");
         int validLength = worldRequired ? 4 : 3;
         World world = null;
@@ -24,7 +24,7 @@ public class Util {
         if (worldRequired) {
             genericError = "Tried to parse location \"" + location + "\" however it is not in the correct format, format should be \"WORLD-NAME X-COORD Y-COORD Z-COORD\" for example \"world 0 64 0\".  Specific error: ";
             world = Bukkit.getWorld(parts[0]);
-            parts = Arrays.copyOfRange(parts, 1, 4);
+            parts = Arrays.copyOfRange(parts, 1, parts.length);
         }
 
         double x; double y; double z;
@@ -36,7 +36,18 @@ public class Util {
             throw new IllegalArgumentException(genericError + "some parts of the location string were not numbers, make sure there are valid x, y and z coordinates after the world name.");
         }
 
-        return new Location(world, x, y, z);
+        Location loc =  new Location(world, x, y, z);
+
+        try {
+            if (parts.length >= 4)
+                loc.setYaw(Float.parseFloat(parts[3]));
+            if (parts.length >= 5)
+                loc.setPitch(Float.parseFloat(parts[4]));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(genericError + "yaw or pitch were not valid numbers.");
+        }
+
+        return loc;
     }
 
 }
